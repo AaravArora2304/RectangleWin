d470be9e203d1c488b0addc3376c6e8488701617
AaravArora2304 <arora.aarav2304@gmail.com>
2026-05-29 01:27:07 -0400

    Update main.go

--- main.go
+++ main.go
@@ -83,22 +83,22 @@ func main() {
 	cycleCornerFuncs := func(i int) { cycleFuncs(cornerFuncs, &cornerFuncTurn, i) }
 
 	hks := []HotKey{
-		(HotKey{id: 1, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: w32.VK_LEFT, callback: func() { cycleEdgeFuncs(0) }}),
-		(HotKey{id: 2, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: w32.VK_RIGHT, callback: func() { cycleEdgeFuncs(1) }}),
-		(HotKey{id: 3, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: w32.VK_UP, callback: func() { cycleEdgeFuncs(2) }}),
-		(HotKey{id: 4, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: w32.VK_DOWN, callback: func() { cycleEdgeFuncs(3) }}),
-		(HotKey{id: 5, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: 0x51, callback: func() { cycleCornerFuncs(0) }}),
-		(HotKey{id: 6, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: 0x45, callback: func() { cycleCornerFuncs(1) }}),
-		(HotKey{id: 7, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: 0x41, callback: func() { cycleCornerFuncs(2) }}),
-		(HotKey{id: 8, mod: MOD_SHIFT | MOD_WIN | MOD_NOREPEAT, vk: 0x44, callback: func() { cycleCornerFuncs(3) }}),
-		(HotKey{id: 50, mod: MOD_SHIFT | MOD_WIN, vk: 0x46 /*F*/, callback: func() {
+		(HotKey{id: 1, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: w32.VK_LEFT, callback: func() { cycleEdgeFuncs(0) }}),
+		(HotKey{id: 2, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: w32.VK_RIGHT, callback: func() { cycleEdgeFuncs(1) }}),
+		(HotKey{id: 3, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: w32.VK_UP, callback: func() { cycleEdgeFuncs(2) }}),
+		(HotKey{id: 4, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: w32.VK_DOWN, callback: func() { cycleEdgeFuncs(3) }}),
+		(HotKey{id: 5, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: 0x51, callback: func() { cycleCornerFuncs(0) }}),
+		(HotKey{id: 6, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: 0x45, callback: func() { cycleCornerFuncs(1) }}),
+		(HotKey{id: 7, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: 0x41, callback: func() { cycleCornerFuncs(2) }}),
+		(HotKey{id: 8, mod: MOD_ALT | MOD_SHIFT | MOD_NOREPEAT, vk: 0x44, callback: func() { cycleCornerFuncs(3) }}),
+		(HotKey{id: 50, mod: MOD_ALT | MOD_SHIFT, vk: 0x46 /*F*/, callback: func() {
 			lastResized = 0 // cause edgeFuncTurn to be reset
 			if err := maximize(); err != nil {
 				fmt.Printf("warn: maximize: %v\n", err)
 				return
 			}
 		}}),
-		(HotKey{id: 60, mod: MOD_ALT | MOD_WIN, vk:w32.VK_RETURN /*C*/, callback: func() {
+		(HotKey{id: 60, mod: MOD_ALT | MOD_SHIFT, vk:w32.VK_RETURN /*C*/, callback: func() {
 			lastResized = 0 // cause edgeFuncTurn to be reset
 			if _, err := resize(w32.GetForegroundWindow(), fillScreen); err != nil {
 				fmt.Printf("warn: maximize: %v\n", err)
